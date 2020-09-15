893799c91c6582d1cf2768e9fe4ed12d75c4f19d
goodlucky68 <wesjfc@msn.com>
2020-09-15 23:44:24 +0800

    testtest

---
+++ src/test/java/com/liuyan/maven/helloword/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package com.liuyan.maven.helloword;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testSayHello() {
+		HelloWorld helloWorld = new HelloWorld();
+		Assert.assertEquals(helloWorld.sayHello(), "hello maven");
+	}
+
+}
